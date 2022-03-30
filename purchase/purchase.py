a0d034d3e447232d3ae0e69b91bf8395dfa2a416
Raimon Esteve <raimonesteve@gmail.com>
2022-03-30 16:25:06 +0200

    Check rule only if _check_access is set and enforce companies rule issue4080

--- purchase/purchase.py
+++ purchase/purchase.py
@@ -1,9 +1,16 @@
 from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 from trytond.modules.html_report.html import HTMLPartyInfoMixin
 
 
 class Purchase(HTMLPartyInfoMixin, metaclass=PoolMeta):
     __name__ = 'purchase.purchase'
+
+    @classmethod
+    def __setup__(cls):
+        super(Purchase, cls).__setup__()
+        cls.html_party.context = {'company': Eval('company')}
+        cls.html_party.depends = ['company']
 
     def get_html_address(self, name):
         return (self.invoice_address and self.invoice_address.id
