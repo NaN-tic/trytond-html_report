b728d08fdf3275aaa44215b8e7564cd1cfc1fd8c
Raimon Esteve <raimonesteve@gmail.com>
2023-01-12 10:48:40 +0100

    Support PyPDF2 version 3.0.0 or less version issue11961

--- engine.py
+++ engine.py
@@ -10,8 +10,12 @@ from datetime import date, datetime, timedelta
 from decimal import Decimal
 from functools import partial
 from io import BytesIO
-from PyPDF2 import PdfMerger, PdfReader
 from urllib.parse import urlparse
+
+try:
+    from PyPDF2 import PdfMerger, PdfReader
+except ImportError:
+    from PyPDF2 import PdfFileMerger as PdfMerger, PdfFileReader as PdfReader
 
 import re
 import barcode
