a8012e9250d43bd13b7c215f8219bc3af93dd201
Raimon Esteve <raimonesteve@gmail.com>
2025-04-28 10:59:53 +0200

    Replace account move number by post number issue13864

--- tests/test_scenario_account_invoice.py
+++ tests/test_scenario_account_invoice.py
@@ -76,11 +76,7 @@ class Test(unittest.TestCase):
 
         # Create Write Off method
         WriteOff = Model.get('account.move.reconcile.write_off')
-        sequence_journal, = Sequence.find(
-            [('sequence_type.name', '=', "Account Journal")], limit=1)
-        journal_writeoff = Journal(name='Write-Off',
-                                   type='write-off',
-                                   sequence=sequence_journal)
+        journal_writeoff = Journal(name='Write-Off', type='write-off')
         journal_writeoff.save()
         writeoff_method = WriteOff()
         writeoff_method.name = 'Rate loss'
