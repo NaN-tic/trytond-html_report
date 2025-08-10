40afc45f50bb41b91992d93c9112ddbc2a904948
Albert Cervera i Areny <albert@nan-tic.com>
2025-08-10 11:50:36 +0200

    Use Many2Many for Function field, following test in 7.8 version that One2Many Function fields require a setter.

--- stock/stock.py
+++ stock/stock.py
@@ -78,7 +78,7 @@ class ShipmentInReturn(HTMLPartyInfoMixin, HTMLReportMixin, metaclass=PoolMeta):
 
 class ShipmentOut(HTMLPartyInfoMixin, HTMLReportMixin, metaclass=PoolMeta):
     __name__ = 'stock.shipment.out'
-    sorted_lines = fields.Function(fields.One2Many('stock.move',
+    sorted_lines = fields.Function(fields.Many2Many('stock.move',
         'line', 'Sorted Lines'), 'get_sorted_lines')
     sorted_keys = fields.Function(fields.Char('key'), 'get_sorted_keys')
     show_lots = fields.Function(fields.Boolean('Show Lots'),
