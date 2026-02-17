ef773dc5bf4fc26708ea02679f3fd663aa7b8ba8
Albert Cervera i Areny <albert@nan-tic.com>
2026-02-17 18:52:02 +0100

    Remove Dominage<>Jinja compatibility classes.

--- company.py
+++ company.py
@@ -1,70 +1,13 @@
 from dominate.util import raw
 from dominate.tags import br, div, span, strong, table, td, th, tr
 
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
-from .dominate import DominateJinjaRenderer
 from .engine import HTMLReportMixin
 
 
 class Company(metaclass=PoolMeta):
     __name__ = 'company.company'
-
-    @classmethod
-    def _get_action_report(cls, name=None):
-        if not name:
-            return None
-        ActionReport = Pool().get('ir.action.report')
-        reports = ActionReport.search([('report_name', '=', name)], limit=1)
-        return reports[0] if reports else None
-
-    def dominate_header(self, name=None, record=None, records=None, data=None):
-        action = self._get_action_report(name)
-        if action and action.html_header_content:
-            return DominateJinjaRenderer.render_template(
-                action,
-                action.html_header_content,
-                record=record,
-                records=records,
-                data=data,
-            )
-        return raw(self.header or '')
-
-    def dominate_footer(self, name=None, record=None, records=None, data=None):
-        action = self._get_action_report(name)
-        if action and action.html_footer_content:
-            return DominateJinjaRenderer.render_template(
-                action,
-                action.html_footer_content,
-                record=record,
-                records=records,
-                data=data,
-            )
-        return raw(self.footer or '')
-
-    def dominate_last_footer(self, name=None, record=None, records=None,
-            data=None):
-        action = self._get_action_report(name)
-        if action and action.html_last_footer_content:
-            return DominateJinjaRenderer.render_template(
-                action,
-                action.html_last_footer_content,
-                record=record,
-                records=records,
-                data=data,
-            )
-        return raw('')
-
-    def dominate_macro(self, signature_name, name=None, record=None,
-            records=None, data=None):
-        action = self._get_action_report(name)
-        return DominateJinjaRenderer.render_macro(
-            action,
-            signature_name,
-            record=record,
-            records=records,
-            data=data,
-        )
 
     @classmethod
     def show_footer(cls, company=None):
